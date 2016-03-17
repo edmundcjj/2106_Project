4161bb9ec90f38c5c1508663697a19853f1e88e6
Edmund Chow <hoed92@gmail.com>
2016-03-18 00:06:33 +0800

    Added Itinerary controller and gateway

---
+++ OneStopTourist Code/OneStopTourist/Controllers/ItineraryController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OneStopTourist.Models;
+using OneStopTourist.DAL;
+
+namespace OneStopTourist.Controllers
+{
+    public class ItineraryController : Controller
+    {
+        private DataGateway<Itineraries> iGateway = new DataGateway<Itineraries>();
+
+        // GET: Itinerary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Itinerary/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Itinerary/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Itinerary/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Itinerary/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Itinerary/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Itinerary/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Itinerary/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
